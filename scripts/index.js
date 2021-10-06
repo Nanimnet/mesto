c562fd498431dd05e7cbafa91915d2ca1d852690
Nanimnet <88799011+Nanimnet@users.noreply.github.com>
2021-10-06 11:39:33 +0200

    Исправить правки с ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,24 @@ const modalWindow = document.querySelector('.popup');
 const modalWindowCloseBtn = document.querySelector('.popup__close');
 const editBtn = document.querySelector('.profile__edit-button');
 const submitBtn = document.querySelector('.popup__btn-submit');
+let nameСhange = document.querySelector('.profile__title');
+let jobСhange = document.querySelector('.profile__description');
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__form-item_el_heading'); // Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector('.popup__form-item_el_subheading');
 
-function toggleModalWindow() {
-    modalWindow.classList.toggle('popup_is-opened');
+function openPopup() {
+    modalWindow.classList.add('popup_is-opened');
+    nameInput.value = nameСhange.textContent;
+    jobInput.value = jobСhange.textContent;
 }
 
-editBtn.addEventListener('click', toggleModalWindow);
-modalWindowCloseBtn.addEventListener('click', toggleModalWindow);
+function closePopup() {
+    modalWindow.classList.remove('popup_is-opened');
+}
 
-
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__form-item_el_heading'); // Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector('.popup__form-item_el_subheading');// Воспользуйтесь инструментом .querySelector()
+editBtn.addEventListener('click', openPopup);
+modalWindowCloseBtn.addEventListener('click', closePopup);
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -24,18 +28,15 @@ function formSubmitHandler (evt) {
                                                 // Так мы можем определить свою логику отправки.
                                                 // О том, как это делать, расскажем позже.
 
-    // Получите значение полей jobInput и nameInput из свойства value
-    nameInput.getAttribute('value');
-    jobInput.getAttribute('value');
     // Выберите элементы, куда должны быть вставлены значения полей
     let nameСhange = document.querySelector('.profile__title');
     let jobСhange = document.querySelector('.profile__description');
     // Вставьте новые значения с помощью textContent
     nameСhange.textContent = nameInput.value;
     jobСhange.textContent = jobInput.value;
+    closePopup()
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler); 
-submitBtn.addEventListener('click', toggleModalWindow);
